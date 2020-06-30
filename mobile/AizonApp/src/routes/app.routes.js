0138a06b71fdb4e75c32fbc30483601aebbe021b
Alessandro T Santos <alessandro.teixeira@gmail.com>
2020-06-30 13:32:35 -0300

    colocando o novo comp de foto na view manager de foto

--- mobile/AizonApp/src/routes/app.routes.js
+++ mobile/AizonApp/src/routes/app.routes.js
@@ -108,12 +108,13 @@ function AppRoutes(){
               options={{ title: 'Certificado' }}
           />
 
+          {/**
           <AppDrawer.Screen
             name="Home"
             component={Home}
             options={{ title: 'Home' }}/>
 
-          {/**
+
           <AppDrawer.Screen
               name="Photo"
               component={FotoCmp}
