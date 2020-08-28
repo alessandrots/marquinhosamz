8e6b10174fada259c074b04808dbea90ba169ead
Alessandro T Santos <alessandro.teixeira@gmail.com>
2020-08-28 17:16:16 -0300

    testando o upload

--- mobile/AizonApp/src/pages/ScanbotImage/index.js
+++ mobile/AizonApp/src/pages/ScanbotImage/index.js
@@ -18,6 +18,9 @@ import { showAlert, checkLicense, } from '../../util/util';
 //import { addList, isEmpty, cleanList} from '../../components/ScannerBot';
 
 import ScanbotSDK from 'react-native-scanbot-sdk';
+import RNFetchBlob from 'rn-fetch-blob';
+
+import base64 from 'react-native-base64'
 
 /**
  *
@@ -44,8 +47,11 @@ import ScanbotSDK from 'react-native-scanbot-sdk';
         // For example, send the pages to the server
 
         if (route.params.pages) {
-          console.log('route ScanbotImage pages = ', route.params.pages);
-          setList(route.params.pages);
+          //console.log('route ScanbotImage pages = ', route.params.pages);
+
+
+
+          showImagesScanbot(route.params.pages);
         }
       }
 
@@ -186,6 +192,16 @@ import ScanbotSDK from 'react-native-scanbot-sdk';
         }
     }
 
+    function showImagesScanbot(pages) {
+      console.log(' showImagesScanbot pages = ', pages);
+      setList(pages);
+
+      for (let i = 0; i < pages.length; i++) {
+          let pageTmp = pages[i];
+          console.log('showImagesScanbot documentImageFileUri = ', pageTmp.documentImageFileUri);
+      }
+  }
+
 
 
     function onGalleryItemClick(page) {
@@ -242,6 +258,9 @@ import ScanbotSDK from 'react-native-scanbot-sdk';
 
             showAlert('PDF with OCR layer created: ' + result.pdfFileUri, 'IMAGE');
         } catch (e) {
+            console.log('====================================');
+            console.log('PDF with OCR layer created error : ', JSON.stringify(e));
+            console.log('====================================');
             showAlert('ERROR: ' + JSON.stringify(e), 'IMAGE');
         } finally {
             hideProgress();
@@ -263,13 +282,139 @@ import ScanbotSDK from 'react-native-scanbot-sdk';
                 compression: binarized ? 'CCITT_T6' : 'ADOBE_DEFLATE', // optional compression. see documentation!
             });
 
-            showAlert('TIFF file created: ' + result.tiffFileUri), 'IMAGE';
+            showAlert('TIFF file created: ' + result.tiffFileUri, 'IMAGE');
         } catch (e) {
-            showAlert('ERROR: ' + JSON.stringify(e)), 'IMAGE';
+            showAlert('ERROR: ' + JSON.stringify(e), 'IMAGE');
         } finally {
             hideProgress();
         }
     }
+
+    /**
+
+       { documentImageSizeLimit: null,
+    documentPreviewImageFileUri: 'file:///storage/emulated/0/Android/data/com.aizonapp/files/my-custom-storage/snapping_pages/91e4ea57-c8c3-4539-a8b9-959183933476/document_image_preview.jpg?minihash=a30bf89bd3f90f8b37d4917e14ce1cbe',
+    documentImageFileUri: 'file:///storage/emulated/0/Android/data/com.aizonapp/files/my-custom-storage/snapping_pages/91e4ea57-c8c3-4539-a8b9-959183933476/document_image.jpg?minihash=dcb8afba0fe7c106b5d1a89b03da5e49',
+    originalPreviewImageFileUri: 'file:///storage/emulated/0/Android/data/com.aizonapp/files/my-custom-storage/snapping_pages/91e4ea57-c8c3-4539-a8b9-959183933476/original_image_preview.jpg?minihash=c22f78a932de41e7afcae9336e7f3a1e',
+    originalImageFileUri: 'file:///storage/emulated/0/Android/data/com.aizonapp/files/my-custom-storage/snapping_pages/91e4ea57-c8c3-4539-a8b9-959183933476/original_image.jpg?minihash=5416ed73e2bf793261eeab3f5e7aad1c',
+    filter: 'NONE',
+    polygon:
+     [ { y: 0.1901647299528122, x: 0.21705426275730133 },
+       { y: 0.17102713882923126, x: 0.7270671725273132 },
+       { y: 0.7781007885932922, x: 0.7861757278442383 },
+       { y: 0.7914243936538696, x: 0.24483203887939453 } ],
+    detectionResult: 'OK_BUT_TOO_SMALL',
+    pageId: '91e4ea57-c8c3-4539-a8b9-959183933476' },
+    //{ name : 'frente', filename : 'frente.jpg', data : RNFetchBlob.wrap(path_to_the_file) },
+    console.log('showImagesScanbot documentImageFileUri = ', pageTmp.documentImageFileUri);
+    //list.length somente os dois primeiros elementos
+     */
+    function upload() {
+      let arrUpl = [];
+      console.log(' upload list = ', list);
+
+      for (let i = 0; i < 2; i++) {
+          let pageTmp = list[i];
+          let objImg  = {};
+          console.log(' RNFetchBlobBase pageTmp = ', pageTmp);
+
+          let resultado = pageTmp.originalImageFileUri.split("?");
+
+          //console.log(' RNFetchBlobBase resultado = ', resultado);
+          if (i == 0) {
+            convertFile(resultado[0]);
+            objImg['name'] = 'frente';
+            objImg['filename'] ='frente.jpg';
+            objImg['data'] = RNFetchBlob.wrap(pageTmp.documentImageFileUri);
+          } else {
+            //convertFile(RNFetchBlob.wrap(pageTmp.documentImageFileUri));
+            convertFile(resultado[0]);
+            objImg['name'] = 'tras';
+            objImg['filename'] ='tras.jpg';
+            objImg['data'] = RNFetchBlob.wrap(pageTmp.documentImageFileUri);
+          }
+          arrUpl.push(objImg);
+      }
+
+      //console.log(' \n RNFetchBlobBase:', arrUpl);
+
+      /**
+      let task = RNFetchBlob.fetch('POST', 'http://45.4.186.2:5000/image/upload4', {
+          'Content-Type' : 'multipart/form-data'
+      }, arrUpl);
+
+      task.then((data) => {
+        console.log('RNFetchBlob sucess = ', data);
+      })
+      .catch((err) => {
+        console.log('RNFetchBlob err = ', data);
+      })
+
+
+      RNFetchBlob.fetch('POST', 'http://myupload.com/upload', {
+          'Content-Type' : 'multipart/form-data'
+      }, [
+   convertFile(pageTmp.documentImageFileUri);   ])
+       */
+    }
+
+    function convertFile (exampleFilePath) {
+      const fs = RNFetchBlob.fs;
+
+      //console.log('RNFetchBlobBase exampleFilePath = ', exampleFilePath);
+
+      //console.log('RNFetchBlobBase base64 = ', RNFetchBlob.base64.encode(exampleFilePath));
+
+      //console.log('RNFetchBlobBase react-native-base64 = ', base64.encode(exampleFilePath));
+      //getBase64(exampleFilePath);
+
+      //let mime = 'data:image/jpeg;base64,'
+      //let mime = 'image/jpeg';
+     /*
+      let metadata = {
+        type: 'image/jpeg'
+      };
+      */
+
+      let mime = 'application/octet-stream';
+
+
+      fs.readFile(exampleFilePath, 'base64')
+        .then((dataF) => {
+          //let blob =  Blob.build(dataF, { type: `${mime};BASE64` })
+          //let blob = new File(exampleFilePath);
+          //let blob = new File([dataF], 'exampleFilePath', `${mime};BASE64`);
+          let blob =  new Blob([dataF], {type: mime, lastModified: new Date()});
+          getBase64(blob);
+        }).catch((err) => {
+          console.log('RNFetchBlobBase err = ', err);
+        })
+
+        /**
+      fs.readFile(exampleFilePath, 'base64')
+        .then(dataF => {
+            //console.log("\n RNFetchBlobBase sucess:", dataF);
+        })
+        .catch((err) => {
+          //console.log('RNFetchBlobBase err = ', err);
+        }) */
+    }
+
+    function getBase64(file) {
+      console.log("\n RNFetchBlobBase file:", file);
+
+     let  reader = new FileReader();
+      //console.log("\n RNFetchBlobBase reader:", reader);
+
+      reader.readAsDataURL(file);
+
+      reader.onload = function () {
+        console.log('RNFetchBlobBase sucess getBase64 = ',reader.result);
+      };
+      reader.onerror = function (error) {
+        console.log('RNFetchBlobBase error getBase64 = ',error);
+      };
+   }
 
     function getMontagemTela() {
         return (
@@ -327,6 +472,16 @@ import ScanbotSDK from 'react-native-scanbot-sdk';
                     <Text style={modal.text}>
                       How would you like to save the pages?
                     </Text>
+
+                    <Text
+                      style={[
+                        modal.button,
+                        modal.actionButton,
+                      ]}
+                      onPress={() => upload()}>
+                      upload
+                    </Text>
+
                     <Text
                       style={[
                         modal.button,
@@ -343,30 +498,9 @@ import ScanbotSDK from 'react-native-scanbot-sdk';
                       onPress={() => onSaveAsPDFWithOCR()}>
                       PDF with OCR
                     </Text>
-                    <Text
-                      style={[
-                        modal.button,
-                        modal.actionButton,
-                      ]}
-                      onPress={() => onSaveAsTIFF(true)}>
-                      TIFF (1-bit B&W)
-                    </Text>
-                    <Text
-                      style={[
-                        modal.button,
-                        modal.actionButton,
-                      ]}
-                      onPress={() => onSaveAsTIFF(false)}>
-                      TIFF (color)
-                    </Text>
-                    <Text
-                      style={[
-                        modal.button,
-                        modal.closeButton,
-                      ]}
-                      onPress={() => onModalClose()}>
-                      Cancel
-                    </Text>
+
+
+
                   </View>
                 </View>
               </Modal>
